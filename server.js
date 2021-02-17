649e065419750bfb74fc7769282c7cc55f16c414
Gaurav Patidar <55226497+gaurav12patidar@users.noreply.github.com>
2021-02-17 13:41:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('.dist/mg'));
+app.use(express.static('./dist/mg'));
 app.get('/*',(req,res)=>
-    res.sendFile('index.html', {root: 'dist/mg'}),
+    res.sendFile('index.html', {root: 'dist/mg/'}),
 );
 app.listen(process.env.PORT || 8080);
